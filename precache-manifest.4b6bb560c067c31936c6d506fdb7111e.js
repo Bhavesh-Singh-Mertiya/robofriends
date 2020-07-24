f72b5d91cbaf8233fbef8d81e3ba7f16748d1d7e
Bhavesh-Singh-Mertiya <26bhaveshsingh@gmail.com>
2020-07-24 08:50:32 +0530

    Updates

---
+++ precache-manifest.4b6bb560c067c31936c6d506fdb7111e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b10816de8ecbb37f8f2f",
+    "url": "/robofriends/static/css/main.444031a5.chunk.css"
+  },
+  {
+    "revision": "b10816de8ecbb37f8f2f",
+    "url": "/robofriends/static/js/main.b77a1d4d.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "d9b98439ec7aa4c0b175",
+    "url": "/robofriends/static/css/2.9545ccce.chunk.css"
+  },
+  {
+    "revision": "d9b98439ec7aa4c0b175",
+    "url": "/robofriends/static/js/2.86a7ce60.chunk.js"
+  },
+  {
+    "revision": "07ee45090c503f714635533547e58d1e",
+    "url": "/robofriends/index.html"
+  }
+];
